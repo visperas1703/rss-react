f1a4c54601961d14623b4173045e4be957807c60
Ivan Lavrenov <visperas1703@gmail.com>
2024-07-08 00:19:19 +0500

    feat: add husky, lint, prettier

---
+++ my-react-app/.eslintrc.cjs
@@ -0,0 +1,26 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2020: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:react/recommended',
+    'plugin:prettier/recommended', 
+  ],
+  ignorePatterns: ['dist'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react-hooks', 'react', 'prettier'],
+  rules: {
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    '@typescript-eslint/no-explicit-any': 'error',
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+  },
+};
